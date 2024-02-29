470968781cdfb523f0e7e0c95f98a8ecefbab047
Nima <nimashahbazi90@gmail.com>
2024-02-29 15:43:54 -0600

    Update remove_script.sh

--- remove_script.sh
+++ remove_script.sh
@@ -1,4 +1,4 @@
-
-ls | grep "*_results" | xargs -I q rm -rf q # removing results directory 
+#! /bin/bash
+ls . | grep "_results" | xargs -I q rm -rf q
 rm -rf experiments
 rm -rf threshold_experiments
